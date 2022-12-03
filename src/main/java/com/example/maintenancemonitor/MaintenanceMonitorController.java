bd6d95323c11198120118cdf49f4ccad36e64679
ChristophTheFirst <ic21b028@technikum-wien-at>
2022-12-03 17:46:33 +0100

    Java-Versionen geändert

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -1,12 +1,12 @@
 package com.example.maintenancemonitor;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+
 public class MaintenanceMonitorController {
 
 
