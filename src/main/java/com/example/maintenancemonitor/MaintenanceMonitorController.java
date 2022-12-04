be75fcc5b5e921c90d4f5d49048320ed9662beab
ChristophTheFirst <ic21b028@technikum-wien-at>
2022-12-04 13:17:15 +0100

    API Message Input + Änderungsdatum

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @RestController
 
 public class MaintenanceMonitorController {
@@ -18,16 +22,23 @@ public class MaintenanceMonitorController {
         if (inputMessage == null) {
             return mainMon.getStatus();
         } else {
-            mainMon.input(inputMessage);
+            DateTimeFormatter dtf =DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            mainMon.input(inputMessage + "<br>last updated " + dtf.format(now) );
             return "OK";
         }
     }
 
         @GetMapping("/api/message/reset")
                 public String reset(){
-            mainMon.input("Everything works  as expected");
+            DateTimeFormatter dtf =DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            mainMon.input("Everything works  as expected" +  "<br>last updated " + dtf.format(now));
             return "OK";
         }
-
+        @GetMapping("/error")
+    public String error(){
+        return"no valid Target, try localhost=:8080/api/message";
+        }
 
 }
